e100d36baaffd6dbbab52216e94364bfcf386f17
Nicole Pendill <coleleeh55@gmail.com>
2026-06-15 14:11:59 -0400

    chore: exclude .worktrees/ from jest test discovery

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.worktrees/'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
