36cd28f2ad12632764813349b9894102a7620f61
Nicole Pendill <coleleeh55@gmail.com>
2026-06-22 11:32:55 -0400

    feat: remove startup gate, delete ConnectPage

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useStore } from './hooks/useStore';
-import ConnectPage from './pages/ConnectPage';
 import DashboardPage from './pages/DashboardPage';
 
 export default function App() {
-  const connection = useStore((s) => s.connection);
-  return connection.connected ? <DashboardPage /> : <ConnectPage />;
+  return <DashboardPage />;
 }
